a25afeca483fc53c4be1c06554db14478b52d8c1
Jarod Solano <Jarod.Solano@gmail.com>
2015-10-01 13:59:39 -0600

    Attempting to return an int and tuple from same function.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,14 +9,22 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        public static int bottomToTopFibaconni(int num1)
+        public static Tuple<int, Array> bottomToTopFibaconni(int num1)
         {
+            // Set desired number from user.
             int n = num1;
+            // This is the smaller of the two sums.
             int n1 = 0;
+            // This is the larger of the two sums.
             int n2 = 0;
+            // This is a placeholder used when iterating to the next n1 and n2 number pair.
             int h1 = 0;
+            // Create Array to prove results.
+            int[] fibArray = new int[num1];
+            // Begin for loop. (We will be starting at 1 for the first position.
             for (int i = 1; i <= n; i++)
             {   
+
                 if (i == 1 || i == 2)
                 {
                     n1 = 1;
@@ -33,8 +41,9 @@ namespace ConsoleApplication1
                     Debug.WriteLine("n2: " + n2);
                     Debug.WriteLine("i: " + i);
                 }
+                fibArray[i] = n2;
             }
-            return n2;
+            return Tuple.Create(n2, fibArray);
         }
 
         static void Main(string[] args)
